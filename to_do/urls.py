1d083381e051d6e41c3407dd487539953199c70c
maccarets <maccarets@gmail.com>
2023-07-26 10:59:58 +0300

    add home page

---
+++ to_do/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from to_do.views import index
+
+urlpatterns = [
+    path("", index, name="index"),
+]
--- to_do/views.py
+++ to_do/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from to_do.models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, "index.html",{"task": tasks})
